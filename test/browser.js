4fed9c09d051e3121a59122f005945fc4d85bffb
achingbrain <alex@achingbrain.net>
2018-05-30 11:43:01 +0100

    feat: add mfs implementation

--- test/browser.js
+++ test/browser.js
@@ -6,8 +6,3 @@ if (isWebWorker) {
   // https://github.com/Joris-van-der-Wel/karma-mocha-webworker/issues/4
   global.MFS_DISABLE_CONCURRENCY = true
 }
-
-require('./cli')
-require('./http-api')
-require('./gateway')
-// require('./core') // get automatically picked up
